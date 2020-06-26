927193988e528a151ea1e21ac64ce531f0ecf88f
Olegus <gulldan1996@gmail.com>
2020-06-26 11:43:59 +0300

    Updates

---
+++ precache-manifest.b1fc8f6c77a9064823cea4442dbed3d5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6e63b7dc4f9ad7a3f436be84785295",
+    "url": "/climate_store/index.html"
+  },
+  {
+    "revision": "d6b5a56e1c8cad8ad910",
+    "url": "/climate_store/static/css/main.96c24361.chunk.css"
+  },
+  {
+    "revision": "6d5a8954f007a842f9bb",
+    "url": "/climate_store/static/js/2.47482f1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/climate_store/static/js/2.47482f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b5a56e1c8cad8ad910",
+    "url": "/climate_store/static/js/main.5a237baa.chunk.js"
+  },
+  {
+    "revision": "41de7365c5ca7124e406",
+    "url": "/climate_store/static/js/runtime-main.c716d433.js"
+  },
+  {
+    "revision": "20392b4a19b13b482a85d7b3c8ee0fd5",
+    "url": "/climate_store/static/media/FTXLA_wifi.20392b4a.jpg"
+  },
+  {
+    "revision": "1110f141d4bd158a6452bdb1e19a0f14",
+    "url": "/climate_store/static/media/Kolag_bora.1110f141.jpg"
+  },
+  {
+    "revision": "9d253cb2133f55cfb27ac8455eed9e2e",
+    "url": "/climate_store/static/media/Smart_kolag.9d253cb2.jpg"
+  },
+  {
+    "revision": "829e0e0a775aeb1ef155f154da21c2aa",
+    "url": "/climate_store/static/media/aux-card.829e0e0a.jpg"
+  },
+  {
+    "revision": "0cb5f36dadf7e16ccd4cd157cef570bb",
+    "url": "/climate_store/static/media/aux-svg.0cb5f36d.png"
+  },
+  {
+    "revision": "1eb15b2172271b26cd63d37797a1c323",
+    "url": "/climate_store/static/media/categoryimage14806051092.1eb15b21.jpg"
+  },
+  {
+    "revision": "cc909696ee7c45804e727573a5038324",
+    "url": "/climate_store/static/media/ch-card.cc909696.jpg"
+  },
+  {
+    "revision": "da28c44baa9acd4c33d215bbe1ac77b0",
+    "url": "/climate_store/static/media/ch.da28c44b.png"
+  },
+  {
+    "revision": "4a796bb4e48f326bd5b64c086873c9ca",
+    "url": "/climate_store/static/media/climate_store.4a796bb4.svg"
+  },
+  {
+    "revision": "c08a0c28cc880ed7909e75e1095b3f12",
+    "url": "/climate_store/static/media/condicioner_gree_practic_pro.c08a0c28.jpg"
+  },
+  {
+    "revision": "aca8c8256c1dc85ebc223b4eea5ff24b",
+    "url": "/climate_store/static/media/gree-card.aca8c825.jpg"
+  },
+  {
+    "revision": "e76b466ec1a9ba94c415c4df4861e650",
+    "url": "/climate_store/static/media/gree.e76b466e.png"
+  },
+  {
+    "revision": "5179b1faec71ae08b9e6c0063f9f197b",
+    "url": "/climate_store/static/media/me-card.5179b1fa.jpg"
+  },
+  {
+    "revision": "92ee2c6ddc64f0b036ae7383a9ab8c19",
+    "url": "/climate_store/static/media/me.92ee2c6d.png"
+  },
+  {
+    "revision": "8357fb2b24baeae76a5bf9de18c9c34c",
+    "url": "/climate_store/static/media/mitsubishi-electric-msz-ef25ve2s-muz-ef25ve.8357fb2b.jpg"
+  },
+  {
+    "revision": "8fcac6e01567301550bcad3b58dbb3b4",
+    "url": "/climate_store/static/media/tcl-card.8fcac6e0.jpg"
+  },
+  {
+    "revision": "9cb7912a7461977a53720b7ef86c42aa",
+    "url": "/climate_store/static/media/tcl.9cb7912a.png"
+  },
+  {
+    "revision": "ef9dae2bd1ee2d3e48ccb5d28a7c006f",
+    "url": "/climate_store/static/media/tcl.ef9dae2b.jpg"
+  },
+  {
+    "revision": "a888fec0a2859e59674a89bc7722e062",
+    "url": "/climate_store/static/media/telegram.a888fec0.svg"
+  },
+  {
+    "revision": "e3b2a3724ad19e68eb8c8901f4a168bb",
+    "url": "/climate_store/static/media/winner_ch-600x60022.e3b2a372.jpg"
+  }
+]);
